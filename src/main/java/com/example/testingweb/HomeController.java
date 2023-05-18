cb3f7fa1d10206e53d8178dc4bd0b3ce99819aad
ruben-pro-cl <91396963+ruben-pro-cl@users.noreply.github.com>
2023-05-18 00:30:53 +0000

    Debería pasar sonar cloud ahora

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
 	@RequestMapping("/")
-	public @ResponseBody String greeting() {
-		String unusedVariable = "Esto es una variable no usada";
+	public @ResponseBody String greeting() {		
 		return "Hello, World";
 	}
 
